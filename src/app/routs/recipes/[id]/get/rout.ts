20826bedac7cf726596b46982194639c2ee569e9
SariGefner <saragefner8008@gmail.com>
2024-11-12 13:58:44 +0200

    change the local changes

--- src/app/routs/recipes/[id]/get/rout.ts
+++ src/app/routs/recipes/[id]/get/rout.ts
@@ -1,1 +1,19 @@
-// im not shure that this is neccessery
+// import { NextRequest, NextResponse } from "next/server";
+// import connectDb from '@/app/lib/db/mongodb'
+// import recipeSchema from '@/app/lib/models/Recipe'
+
+// export async function GET(
+//     req: NextRequest,
+//     { params }: { params: { id: string } }
+// ) {
+//     try {
+//         await connectDb();
+//         const recipe = await recipeSchema.findById(params.id);
+
+//         if (!recipe) {
+//             return NextResponse.json({ error: 'user not found' }, { status: 404 });
+//         }
+//         return 
+//     }
+
+// }
