f530afd1d4901119e7257d6656d0add40779be29
DraymanR <dr0548424205@gmail.com>
2024-11-18 14:29:29 +0200

    18.11.2024 9

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ['infobase.click', 'example.com'],
+//   },
+// };
+
+// export default nextConfig;
+export default {
+  images: {
+    domains: ['infobase.click', 'example.com'],
+  },
+};
