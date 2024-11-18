fc494a9ce5cdf9717145742024f1d078494ba51f
DraymanR <dr0548424205@gmail.com>
2024-11-18 13:57:16 +0200

    18.11.2024 5

--- src/app/api/category/get/[categoryName]/route.ts
+++ src/app/api/category/get/[categoryName]/route.ts
@@ -2,13 +2,12 @@ import { NextResponse } from 'next/server';
 import connectDb from '@/app/lib/db/connectDb'; 
 import { Category } from '@/app/lib/models/Category';
 
-export async function GET(req: Request, { params }: { params: { categoryName: string } }) {
+export async function GET(req: Request, context: { params: { categoryName: string } }) {
     try {
       await connectDb();
-      const { categoryName } = params;
+      const { categoryName } = context.params; // Use context.params directly
   
-      const category = await Category.findOne({ categoryName:categoryName }).populate('recipes');
-  
+      const category = await Category.findOne({ categoryName: categoryName }).populate('recipes');
   
       if (!category) {
         return NextResponse.json(
@@ -17,17 +16,16 @@ export async function GET(req: Request, { params }: { params: { categoryName: st
         );
       }
   
-  
       return NextResponse.json(
-        { message: 'categorys recipes fetched successfully',category: category.recipes },
+        { message: 'category recipes fetched successfully', category: category.recipes },
         { status: 200 }
       );
     } catch (error) {
-      console.error('Error fetching user and recipes by categoryName:', error);
+      console.error('Error fetching recipes by categoryName:', error);
   
       return NextResponse.json(
-        { error: 'Failed to fetch  recipes', details: error },
+        { error: 'Failed to fetch recipes', details: error },
         { status: 500 }
       );
     }
-  }
+}
