4b5f74010c082dc2417b62a4828540b3cd608fea
DraymanR <dr0548424205@gmail.com>
2024-11-18 15:02:34 +0200

    111

--- src/app/store/useRecipesStore.ts
+++ src/app/store/useRecipesStore.ts
@@ -1,46 +1,46 @@
-import { create } from 'zustand';
-import { IRecipe } from '@/app/types/recipes';
+// import { create } from 'zustand';
+// import { IRecipe } from '@/app/types/recipes';
 
-interface RecipesState {
-  recipes: IRecipe[];
-  addRecipe: (recipe: IRecipe) => void;
-  removeRecipe: (recipe: IRecipe) => void;
-  updateRecipe: (recipe: IRecipe) => void;
-}
+// interface RecipesState {
+//   recipes: IRecipe[];
+//   addRecipe: (recipe: IRecipe) => void;
+//   removeRecipe: (recipe: IRecipe) => void;
+//   updateRecipe: (recipe: IRecipe) => void;
+// }
 
-export const useRecipesStore = create<RecipesState>()((set) => ({
-  recipes: [],
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-  removeRecipe: (recipe) =>
-    set((state) => ({
-      recipes: state.recipes.filter((r) => r.name !== recipe.name),
-    })),
-  updateRecipe: (recipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((r) => (r.name === recipe.name ? recipe : r)),
-    })),
-}));
+// export const useRecipesStore = create<RecipesState>()((set) => ({
+//   recipes: [],
+//   addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+//   removeRecipe: (recipe) =>
+//     set((state) => ({
+//       recipes: state.recipes.filter((r) => r.name !== recipe.name),
+//     })),
+//   updateRecipe: (recipe) =>
+//     set((state) => ({
+//       recipes: state.recipes.map((r) => (r.name === recipe.name ? recipe : r)),
+//     })),
+// }));
 
-export default useRecipesStore;import { create } from 'zustand';
-import { IRecipe } from '@/app/types/recipes'
-interface RecipesState {
-  recipes: IRecipe[];
-  addRecipe: (recipe: IRecipe) => void;
-  removeRecipe: (recipe: IRecipe) => void;
-  updateRecipe: (recipe: IRecipe) => void;
-}
+// export default useRecipesStore;import { create } from 'zustand';
+// import { IRecipe } from '@/app/types/recipes'
+// interface RecipesState {
+//   recipes: IRecipe[];
+//   addRecipe: (recipe: IRecipe) => void;
+//   removeRecipe: (recipe: IRecipe) => void;
+//   updateRecipe: (recipe: IRecipe) => void;
+// }
 
-export const useRecipesStore = create<RecipesState>()((set) => ({
-  recipes: [],
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-  removeRecipe: (recipe) =>
-    set((state) => ({
-      recipes: state.recipes.filter((r) => r.name !== recipe.name),
-    })),
-  updateRecipe: (recipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((r) => (r.name === recipe.name ? recipe : r)),
-    })),
-}));
+// export const useRecipesStore = create<RecipesState>()((set) => ({
+//   recipes: [],
+//   addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+//   removeRecipe: (recipe) =>
+//     set((state) => ({
+//       recipes: state.recipes.filter((r) => r.name !== recipe.name),
+//     })),
+//   updateRecipe: (recipe) =>
+//     set((state) => ({
+//       recipes: state.recipes.map((r) => (r.name === recipe.name ? recipe : r)),
+//     })),
+// }));
 
-export default useRecipesStore;
+// export default useRecipesStore;
