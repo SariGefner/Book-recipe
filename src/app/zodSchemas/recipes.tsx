59ed17f7e7c8650093a26c61d8a490cde78f52d8
DraymanR <dr0548424205@gmail.com>
2024-11-19 11:28:41 +0200

    it work! 3

--- src/app/zodSchemas/recipes.tsx
+++ src/app/zodSchemas/recipes.tsx
@@ -1,13 +1,13 @@
-import { z } from 'zod';
+// import { z } from 'zod';
 
-export const RecipeSchema = z.object({
-  image: z.string().url(),
-  name: z.string().min(1, "Recipe name is required"),
-  categoryName: z.string().min(1, "Category name is required"),
-  ingredients: z.array(z.string()).nonempty("Ingredients cannot be empty"),
-  favorite: z.boolean().optional(),
-  preparationInstructions: z.string().min(1, "Instructions are required"),
-  preparationDays: z.number().max(100, "Preparation days must be 100 or less")
-});
+// export const RecipeSchema = z.object({
+//   image: z.string().url(),
+//   name: z.string().min(1, "Recipe name is required"),
+//   categoryName: z.string().min(1, "Category name is required"),
+//   ingredients: z.array(z.string()).nonempty("Ingredients cannot be empty"),
+//   favorite: z.boolean().optional(),
+//   preparationInstructions: z.string().min(1, "Instructions are required"),
+//   preparationDays: z.number().max(100, "Preparation days must be 100 or less")
+// });
 
-export type Recipe = z.infer<typeof RecipeSchema>;
+// export type Recipe = z.infer<typeof RecipeSchema>;
